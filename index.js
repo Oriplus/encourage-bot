0f7306b678fbe9896f1f7aef4ea2307f08679c40
Oriana Morillo <>
2022-06-16 01:50:04 +0000

    changes for replit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const fs = require('fs');
+const path = require('path');
 const config = require("./config/config.js");
+const { Client, Intents } = require('discord.js');
+const keepAlive = require("./server");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
@@ -17,4 +19,5 @@ for (const file of eventFiles) {
 	}
 }
 
-client.login(config.token);
+keepAlive();
+client.login(config.config.token);
